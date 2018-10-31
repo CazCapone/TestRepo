825d8bfb422fa015b5b82721c5a047a2bf19a309
Joseph Duclos <43616909+CazCapone@users.noreply.github.com>
2018-10-31 10:12:30 -0700

    yet another

--- JavaGitHubTest/src/javagithubtest/testtesttest.java
+++ JavaGitHubTest/src/javagithubtest/testtesttest.java
@@ -11,4 +11,10 @@ package javagithubtest;
  */
 public class testtesttest {
     
+    
+    public static void main (String[] args){
+        
+        System.out.println("This is my test.");
+        
+    }
 }
